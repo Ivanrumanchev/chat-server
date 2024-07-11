abf83cfc34d8565367a4869e678d5d7cd2ac8b14
Ivan Rumyantsev <rumyantsevin@multibonus.ru>
2024-07-11 17:19:55 +0300

    feature/1/client-server

---
+++ cmd/grpc_client/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	desc "github.com/Ivanrumanchev/chat-server/pkg/chat_v1"
+)
+
+const (
+	address = "localhost:50051"
+	chatID  = int64(3)
+	from    = "Vasya"
+	text    = "text"
+)
+
+var usernames = []string{"Vasya", "Petya"}
+var timestamp = timestamppb.New(time.Now())
+
+func closeConn(conn *grpc.ClientConn) {
+	if err := conn.Close(); err != nil {
+		log.Printf("Error closing connection: %v", err)
+	}
+}
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to server: %v", err)
+	}
+	defer closeConn(conn)
+
+	c := desc.NewChatV1Client(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	createResponse, err := c.Create(ctx, &desc.CreateRequest{Usernames: usernames})
+	if err != nil {
+		log.Fatalf("failed to create chat by id: %v", err)
+	}
+
+	log.Printf(color.RedString("Create Chat info:\n"), color.GreenString("%+v", createResponse.GetId()))
+
+	deleteResponse, err := c.Delete(ctx, &desc.DeleteRequest{Id: chatID})
+	if err != nil {
+		log.Fatalf("failed to delete chat by id: %v", err)
+	}
+
+	log.Printf(color.RedString("Delete Chat info:\n"), color.GreenString("%+v", deleteResponse))
+
+	sendMessageResponse, err := c.SendMessage(ctx, &desc.SendMessageRequest{From: from, Text: text, Timestamp: timestamp})
+	if err != nil {
+		log.Fatalf("failed to send message chat by id: %v", err)
+	}
+
+	log.Printf(color.RedString("Chat info:\n"), color.GreenString("%+v", sendMessageResponse))
+}
--- go.mod
+++ go.mod
@@ -1,10 +1,23 @@
-module github.com/olezhek28/microservices_course_boilerplate
+module github.com/Ivanrumanchev/chat-server
 
-go 1.20
+go 1.21
+
+toolchain go1.22.3
 
 require (
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require (
+	github.com/brianvoe/gofakeit v3.18.0+incompatible // indirect
 	github.com/fatih/color v1.15.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.4.0 // indirect
 )
